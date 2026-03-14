45886ed0bddf71ac743714e77c6d0f79f3b86a43
Nitish Kumar <163245178+nitish-kumar3@users.noreply.github.com>
2026-03-14 22:12:59 +0530

    Update user.controller.js

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -5,7 +5,6 @@ import createTokenAndSaveCookie from "../jwt/generateToken.js";
 
 
 
-
 export const signup = async (req, res) => {
   const { fullname, email, password, confirmPassword } = req.body;
   try {
