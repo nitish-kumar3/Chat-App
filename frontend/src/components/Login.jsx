7aafb8cc5f23672920763c0245b28c2b5a81c598
Nitish Kumar <163245178+nitish-kumar3@users.noreply.github.com>
2026-04-04 22:53:59 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -5,6 +5,7 @@ import { useAuth } from "../context/AuthProvider";
 import { Link } from "react-router-dom";
 import toast from "react-hot-toast";
 
+
 function Login() {
   const [authUser, setAuthUser] = useAuth();
 
